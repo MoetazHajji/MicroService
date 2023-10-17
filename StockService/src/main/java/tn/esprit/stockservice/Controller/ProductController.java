aa5c9dfedfe9c4ce37959c290027f9a780eb6363
moetazhajji <moetaz.hajji@esprit.tn>
2023-10-17 14:59:06 +1300

    adding other congifuration to docker compose

--- StockService/src/main/java/tn/esprit/stockservice/Controller/ProductController.java
+++ StockService/src/main/java/tn/esprit/stockservice/Controller/ProductController.java
@@ -1,10 +1,7 @@
 package tn.esprit.stockservice.Controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tn.esprit.stockservice.Entity.Product;
 import tn.esprit.stockservice.Interface.IProductService;
 import tn.esprit.stockservice.Mapper.ProductMapper;
@@ -15,17 +12,34 @@ import java.util.stream.Collectors;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/stock")
+@RequestMapping("/product")
 public class ProductController {
     IProductService productService;
     @PostMapping()
-    public ProductDto addProduct(ProductDto productDto) {
+    public ProductDto addProduct(@RequestBody ProductDto productDto) {
         final Product product = ProductMapper.mapToEntity(productDto);
         return ProductMapper.mapToDto(productService.addProduct(product));
     }
-    @GetMapping
+    @GetMapping()
     public Set<ProductDto> getAllProduct(){
         final Set<Product> productSet = productService.getAllProduct();
         return productSet.stream().map(p -> ProductMapper.mapToDto(p)).collect(Collectors.toSet());
     }
+
+    @PutMapping()
+    public ProductDto modifyProduct(@RequestBody ProductDto productDto) {
+        final Product product = ProductMapper.mapToEntity(productDto);
+        return ProductMapper.mapToDto(productService.modifyProduct(product));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable("id") Long productId) {
+        productService.deleteProduct(productId);
+    }
+
+    @GetMapping("/{id}")
+    public Product getProduct(@PathVariable("id") Long productId) {
+        return productService.getProductById(productId);
+    }
+
 }
