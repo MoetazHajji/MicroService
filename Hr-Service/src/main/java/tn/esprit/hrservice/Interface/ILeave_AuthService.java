9f5f38120e8a820aa4cccba04692eed094e16cf4
JallouzN <nesrine.jallouz@esprit.tn>
2023-10-25 00:55:52 +0200

    updated project

--- Hr-Service/src/main/java/tn/esprit/hrservice/Interface/ILeave_AuthService.java
+++ Hr-Service/src/main/java/tn/esprit/hrservice/Interface/ILeave_AuthService.java
@@ -2,26 +2,14 @@ package tn.esprit.hrservice.Interface;
 
 
 
+import org.springframework.stereotype.Service;
 import tn.esprit.hrservice.Dto.Leave_AuthorizationDto;
 import tn.esprit.hrservice.Entity.Leave_Authorization;
 
 import java.util.Date;
 import java.util.List;
-
+@Service
 public interface ILeave_AuthService {
 
-    //s public Leave_AuthorizationDto updateLeaveAuth(Leave_Authorization la, Long idAs);
-    public Leave_AuthorizationDto updateLeaveAuth(Leave_AuthorizationDto la, Long idA);
-    public void deleteLeaveAuth(Long idLA);
-    public List<Leave_AuthorizationDto> retrieveAllLeaveAuths();
-    public Leave_AuthorizationDto retrieveLeaveAuthById(Long idLA);
-    public Leave_AuthorizationDto addAndAssignLAToAccount(Leave_Authorization la, Long idA);
-    public Leave_AuthorizationDto assignLAToAccount(Long idLA, Long idAccount);
-    public void countingRemainingdays();
-    public void checkAndUpdateLeaveStatus();
-    public List<Leave_AuthorizationDto> retrieveLAByAccountId(Long idA);
-    public List<Leave_AuthorizationDto> retrieveLeaveAuthByPeriod(Date startDate, Date endDate);
-
-    //public List<Float> extractNumbersFromString(String inputString);
 
 }
